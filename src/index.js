8daba835efb774fb1ffac0c0a922ce0dc172fdd1
higheditss96 <higheditss96@gmail.com>
2025-11-07 19:42:56 +0200

    initial highstats overlay

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Overlay from "./Overlay";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Overlay />
+  </React.StrictMode>
+);
