823646ca3b7b26b25520ddb5a4c813019aadd150
higheditss96 <higheditss96@gmail.com>
2025-11-12 00:39:00 +0200

    Fix API routes for Vercel

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,6 +1,5 @@
 export default async function handler(req, res) {
   const authUrl = new URL("https://kick.com/oauth/authorize");
-
   authUrl.searchParams.set("response_type", "code");
   authUrl.searchParams.set("client_id", process.env.KICK_CLIENT_ID);
   authUrl.searchParams.set("redirect_uri", process.env.KICK_REDIRECT_URI);
