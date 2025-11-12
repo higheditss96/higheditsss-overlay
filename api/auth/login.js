96151de5b4a2ac348e5b15f2feee9a54a806708d
higheditss96 <higheditss96@gmail.com>
2025-11-12 02:04:51 +0200

    Fix Kick OAuth URL

--- api/auth/login.js
+++ api/auth/login.js
@@ -7,6 +7,8 @@ export default async function handler(req, res) {
     force_verify: "true"
   });
 
-  const authURL = `https://kick.com/oauth/authorize?${params.toString()}`;
+  // noul URL corect pentru Kick OAuth
+  const authURL = `https://kick.com/api/v1/oauth/authorize?${params.toString()}`;
+
   return res.redirect(authURL);
 }
